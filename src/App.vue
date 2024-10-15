1fb9d429737da07a843cce58a01626f3dff20dcb
Kudryavtsev Maxim <kumaxim@users.noreply.github.com>
2024-10-15 22:17:35 +0300

    Add footer with copyright

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import {RouterView} from 'vue-router'
 import TheHeader from '@/components/TheHeader.vue'
 import ToastStack from '@/components/twbs/Toast.vue'
+import TheFooter from '@/components/TheFooter.vue'
 </script>
 
 <template>
   <TheHeader/>
   <RouterView/>
   <ToastStack/>
+  <TheFooter/>
 </template>
 
 <style scoped lang="scss">
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const year = new Date().getFullYear()
+</script>
+
+<template>
+  <footer class="pb-2 pt-4 mt-auto">
+    <div class="border-1 border-top pt-4">
+      <p class="text-center text-body-secondary">
+        &copy; {{ year <= 2024 ? 2024 : `2024 - ${year}` }} Кудрявцев Максим. Все права защищены.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+
+</style>
